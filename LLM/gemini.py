37e51ee3d097bccca572635cf11615f99dec7955
danylookhrin2 <danylo.okhrin@tum.de>
2024-11-10 19:05:35 +0100

    created jpyter notebook and data update

--- LLM/gemini.py
+++ LLM/gemini.py
@@ -7,8 +7,5 @@ def send_requestGemini(model, system_promt, text):
     model = genai.GenerativeModel(model)
     response = model.generate_content(system_promt + "\n" + text, safety_settings={
                                                             HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
-                                                            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
-                                                            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
-                                                            HarmCategory.HARM_CATEGORY_UNSPECIFIED:HarmBlockThreshold.BLOCK_NONE,
-                                                            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT:HarmBlockThreshold.BLOCK_NONE})
+                                                            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE})
     return response.text
