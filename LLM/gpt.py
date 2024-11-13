9c406370dd7cd6633f3bda5413497ed664ed9d50
danylookhrin2 <danylo.okhrin@tum.de>
2024-11-13 17:19:32 +0100

    added parallel processing and data update

--- LLM/gpt.py
+++ LLM/gpt.py
@@ -1,4 +1,5 @@
 # setx OPENAI_API_KEY ""
+# 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
 from openai import OpenAI
 
 client = OpenAI()
